ba65a4a3cf098ef2595cbc20e5b4dac568ae3120
Dmitry.Skiba <Dmitry.Skiba@c17a5bc7-4154-0410-babe-fd7bbd07cc42>
2008-08-17 17:30:32 +0000

    added more stub classes

--- src/android/graphics/Matrix.java
+++ src/android/graphics/Matrix.java
@@ -19,6 +19,7 @@ import javolution.lang.Enum;
 
 public class Matrix {
 	
+	// is this needed?
 	public static final class ScaleToFit extends Enum {
 
 		public static final ScaleToFit 
@@ -59,7 +60,8 @@ public class Matrix {
 
 	public boolean isIdentity() {return false;}
 
-	public boolean rectStaysRect() {return false;}
+	// always true, as we don't support skew & rotation? 
+	public boolean rectStaysRect() {return true;}
 
 	public void set(Matrix src) {}
 
@@ -67,84 +69,69 @@ public class Matrix {
 
 	public void reset() {}
 
-	public void setTranslate(float dx, float dy) {}
+	// IGNORE (-ScaleAnimation, -ScaleLayout, -ImageView's ScaleType.CENTER_CROP,
+	// -Gallery
+	//public void setScale(float sx, float sy, float px, float py) {}
+	//public void setScale(float sx, float sy) {}
 
-	public void setScale(float sx, float sy, float px, float py) {}
+	// not used
+	//public boolean preScale(float sx, float sy, float px, float py) {return false;}
+	//public boolean preScale(float sx, float sy) {return false;}
+	//public boolean postScale(float sx, float sy, float px, float py) {return false;}
+	//public boolean postScale(float sx, float sy) {return false;}
 
-	public void setScale(float sx, float sy) {}
+	// IGNORE (-RotateAnimation)
+	//public void setRotate(float degrees, float px, float py) {}
+	//public void setRotate(float degrees) {}
 
-	public void setRotate(float degrees, float px, float py) {}
+	// not used
+	//public boolean preRotate(float degrees, float px, float py) {return false;}
+	//public boolean preRotate(float degrees) {return false;}
+	//public boolean postRotate(float degrees, float px, float py) {return false;}
+	//public boolean postRotate(float degrees) {return false;}
+	
+	// not used
+	//public void setSinCos(float sinValue, float cosValue, float px, float py) {}
+	//public void setSinCos(float sinValue, float cosValue) {}
 
-	public void setRotate(float degrees) {}
+	// not used
+	//public void setSkew(float kx, float ky, float px, float py) {}
+	//public void setSkew(float kx, float ky) {}
 
-	public void setSinCos(float sinValue, float cosValue, float px, float py) {}
-
-	public void setSinCos(float sinValue, float cosValue) {}
-
-	public void setSkew(float kx, float ky, float px, float py) {}
-
-	public void setSkew(float kx, float ky) {}
-
+	// not used
+	//public boolean preSkew(float kx, float ky, float px, float py) {return false;}
+	//public boolean preSkew(float kx, float ky) {return false;}
+	//public boolean postSkew(float kx, float ky, float px, float py) {return false;}
+	//public boolean postSkew(float kx, float ky) {return false;}
+	
 	public boolean setConcat(Matrix a, Matrix b) {return false;}
-
-	public boolean preTranslate(float dx, float dy) {return false;}
-
-	public boolean preScale(float sx, float sy, float px, float py) {return false;}
-
-	public boolean preScale(float sx, float sy) {return false;}
-
-	public boolean preRotate(float degrees, float px, float py) {return false;}
-
-	public boolean preRotate(float degrees) {return false;}
-
-	public boolean preSkew(float kx, float ky, float px, float py) {return false;}
-
-	public boolean preSkew(float kx, float ky) {return false;}
-
 	public boolean preConcat(Matrix other) {return false;}
-
-	public boolean postTranslate(float dx, float dy) {return false;}
-
-	public boolean postScale(float sx, float sy, float px, float py) {return false;}
-
-	public boolean postScale(float sx, float sy) {return false;}
-
-	public boolean postRotate(float degrees, float px, float py) {return false;}
-
-	public boolean postRotate(float degrees) {return false;}
-
-	public boolean postSkew(float kx, float ky, float px, float py) {return false;}
-
-	public boolean postSkew(float kx, float ky) {return false;}
-
 	public boolean postConcat(Matrix other) {return false;}
 
-	public boolean setRectToRect(RectF src, RectF dst, ScaleToFit stf) {return false;}
+	public void setTranslate(float dx, float dy) {}
+	public boolean preTranslate(float dx, float dy) {return false;}
+	public boolean postTranslate(float dx, float dy) {return false;}
 
-	public boolean setPolyToPoly(float src[], int srcIndex, float dst[], int dstIndex, int pointCount) {return false;}
-
-	public boolean invert(Matrix inverse) {return false;}
+	// IGNORE (used in ImageView)
+	//public boolean setRectToRect(RectF src, RectF dst, ScaleToFit stf) {return false;}
+	
+	// not used
+	//public boolean setPolyToPoly(float src[], int srcIndex, float dst[], int dstIndex, int pointCount) {return false;}
+	//public float mapRadius(float radius) {return 0;}
+	//public boolean invert(Matrix inverse) {return false;}
 
 	public void mapPoints(float dst[], int dstIndex, float src[], int srcIndex, int pointCount) {}
-
-	public void mapVectors(float dst[], int dstIndex, float src[], int srcIndex, int vectorCount) {}
-
 	public void mapPoints(float dst[], float src[]) {}
-
-	public void mapVectors(float dst[], float src[]) {}
-
 	public void mapPoints(float pts[]) {}
 
+	public void mapVectors(float dst[], int dstIndex, float src[], int srcIndex, int vectorCount) {}
+	public void mapVectors(float dst[], float src[]) {}
 	public void mapVectors(float vecs[]) {}
 
 	public boolean mapRect(RectF dst, RectF src) {return false;}
-
 	public boolean mapRect(RectF rect) {return false;}
 
-	public float mapRadius(float radius) {return 0;}
-
 	public void getValues(float values[]) {}
-
 	public void setValues(float values[]) {}
 
 	public String toString() {return null;}
